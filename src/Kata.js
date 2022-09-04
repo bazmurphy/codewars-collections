cbebcf29b2cb4d11d7b510b987cd92191eda3dec
bazmurphy <bazmurphy@gmail.com>
2022-09-04 08:50:56 +0100

    collapsable containers, and conditional url links

--- src/Kata.js
+++ src/Kata.js
@@ -20,17 +20,15 @@ const Kata = (props) => {
     
     return (
         <div className="kata-container">
-            <div className="kata-sub-container-one">
+            <div className="kata-sub-container">
                 <div className="collection-number">{props.collectionNumber} / {props.collectionTotal}</div>
                 <div className="kata-level">{kataState ? Math.abs(kataState.rank.id) : null} kyu</div>
                 <a className="kata-title-and-link" href={`https://www.codewars.com/kata/${props.kataId}`} target="_blank" rel="noreferrer">{kataState ? kataState.name : null}</a>
-            </div>
-            <div className="kata-sub-container-two">
-                {props.kataComplete ? <img className="kata-complete" src="./complete.png" alt="js logo green" /> : <img className="kata-complete" src="./incomplete.png" alt="js logo white"/>}
+                {props.kataComplete ? <img className="kata-complete-image" src="./complete.png" alt="js logo green" /> : <img className="kata-complete" src="./incomplete.png" alt="js logo white"/>}
                 {props.kataComplete ? <div className="kata-complete-status-true">Completed</div> : <div className="kata-complete-status-false">Uncompleted</div>}
-                <a className="kata-my-link" href={`https://github.com/bazmurphy/codewars/blob/main/${kataState ? Math.abs(kataState.rank.id) : null}kyu/${props.kataId}.js`} target="_blank" rel="noreferrer">My solution on GitHub</a>
+                <a className="kata-my-link" href={props.kataComplete ? (`https://github.com/bazmurphy/codewars/blob/main/${kataState ? Math.abs(kataState.rank.id) : null}kyu/${props.kataId}.js`) : null} target="_blank" rel="noreferrer">My solution on GitHub</a>
+                <div className="kata-id">ID: {props.kataId}</div>
             </div>
-            <div className="kata-id">ID: {props.kataId}</div>
         </div>
     );
 }
